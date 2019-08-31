5b0bbeaed5eba18cef3a22d71c7949140d22a340
reva-compass <rkandoji@compass.com>
2019-08-31 17:05:39 -0400

    performing intial sql queries

--- src/main/scala/com/urbancompass/data/pipeline/flink/TestJoiner.scala
+++ src/main/scala/com/urbancompass/data/pipeline/flink/TestJoiner.scala
@@ -38,8 +38,8 @@ object TestJoiner {
 
   def main(args: Array[String]) {
 
-//    val logger = LoggerFactory.getLogger(FlinkJoiner.getClass)
-//    logger.info("### Hello from flink joiner ")
+    //    val logger = LoggerFactory.getLogger(FlinkJoiner.getClass)
+    //    logger.info("### Hello from flink joiner ")
 
     println("### Entering Test Joiner")
     /*
@@ -48,7 +48,8 @@ object TestJoiner {
 
     2019-07-22: Not currently using this code
      */
-    val messageSchema = """
+    val messageSchema =
+      """
     {
       "name": "pipeline_message",
       "type": "record",
@@ -61,11 +62,6 @@ object TestJoiner {
       ]
     }
     """
-    //@AvroName("pipeline_message")
-    //@AvroNamespace("pipeline")
-    //case class kafkaData(trace_id: String, data_version: String, ts_created_at: String, payload: String)
-    //val avroSchema = AvroSchema[kafkaData]
-    //println("Avro Schema: " + avroSchema)
 
     /*
      Checking input parameters
@@ -83,17 +79,10 @@ object TestJoiner {
     val tEnv = TableEnvironment.getTableEnvironment(env)
     env.setStateBackend(new FsStateBackend("file:///Users/rkandoji/Documents/Software/flink-1.8.1/statebackend"))
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-    //env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
-    //env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime)
     env.setParallelism(1)
 
     env.getConfig.enableForceAvro()
     env.getConfig.disableForceKryo()
-
-    /*
-     Run on Mutable List
-     */
-    //runStaticDataStream()
 
 
     /*
@@ -104,45 +93,24 @@ object TestJoiner {
     // Serializers used for reading topics
     val serdeSchema = new SimpleStringSchema
     val jsonDeserdeSchema = new JSONKeyValueDeserializationSchema(true)
-
-    //val avroDeserdeSchema = AvroDeserializationSchema.forGeneric(avroSchema)
-    //val avroDeserdeSchema = new MyAvroDeserializationSchema[kafkaData](classOf[kafkaData])
-    //val deserializer = new KeyedDeserializationSchema[String](serdeSchema)
-
-    //val binaryDeserdeSchema = new AbstractDeserializationSchema[Array[Byte]](){
-    // override def deserialize(bytes: Array[Byte]): Array[Byte] = java.util.Base64.getDecoder.decode(bytes)
-    //}
-    //val binaryAvroDeserdeSchema = new BinaryAvroDeserializationSchema[GenericRecord](classOf[GenericRecord])
-
-    /*
-     Consumers
-     */
-    //val kafkaListingsConsumer = new FlinkKafkaConsumer(kafkaListingsTopic, binaryAvroDeserdeSchema, consumerProps)
-    //val kafkaListingsConsumer = new FlinkKafkaConsumer(kafkaListingsTopic, avroDeserdeSchema, consumerProps)
     val kafkaListingsConsumer = new FlinkKafkaConsumer[ObjectNode](kafkaListingsTopic, jsonDeserdeSchema, consumerProps)
     kafkaListingsConsumer.setStartFromEarliest()
-    //kafkaListingsConsumer.assignAscendingTimestamps(_.getCreationTime)
-    //  kafkaListingsConsumer.assignTimestampsAndWatermarks(new CustomTimestampExtractor[ObjectNode]())
 
     val kafkaAgentsConsumer = new FlinkKafkaConsumer[ObjectNode](kafkaAgentsTopic, jsonDeserdeSchema, consumerProps)
     kafkaAgentsConsumer.setStartFromEarliest()
-  //  kafkaAgentsConsumer.assignTimestampsAndWatermarks(new CustomTimestampExtractor[ObjectNode]())
 
     val rowListingType = new RowTypeInfo(
-      Types.STRING, // trace_id
-      Types.STRING, // data_version
-      Types.STRING, // ts_created_at
-      Types.STRING, // __raw_ingest_start_time__
-      Types.STRING, // __uc_id_sha__
-      Types.STRING, // Listing ID
-      Types.STRING, // Agent ID
-      Types.STRING, // Office ID
-      Types.STRING, // payload
-      Types.STRING, // payload
-      Types.STRING, // Agent ID
-      Types.STRING, // Office ID
-      Types.STRING, // payload
-      Types.STRING // payload
+      Types.STRING, // earnest_$_payable_to
+      Types.STRING, // status_change_date
+      Types.STRING, // inclusions
+      Types.STRING, // county
+      Types.STRING, // agent_id
+      Types.STRING, // terms_offered
+      Types.STRING, // nbr_of_acres
+      Types.STRING, // colisting_member_url
+      Types.STRING, // colist_agent_id
+      Types.STRING, // list_office_board_code
+      Types.STRING // list_207
     )
 
     def listingMapper(record: ObjectNode): Row = {
@@ -156,7 +124,7 @@ object TestJoiner {
         obj.get("County").asText(),
         obj.get("Agent ID").asText(),
         obj.get("Terms Offered").asText(),
-        obj.get("Nbr of Acres"),
+        obj.get("Nbr of Acres").asText(),
         obj.get("CoListingMemberUrl").asText(),
         obj.get("CoList Agent ID").asText(),
         obj.get("List Office Board Code").asText(),
@@ -164,12 +132,9 @@ object TestJoiner {
       )
     }
 
-    //    val rowCombinedImageType = new RowTypeInfo(Types.STRING, Types.STRING, Types.SQL_TIMESTAMP)
-
-   val listingsStream = env.addSource(kafkaListingsConsumer).map(x => listingMapper(x))(rowListingType)
-    //listingsStream.assignAscendingTimestamps(_.getField(2).asInstanceOf[Long])
+    val listingsStream = env.addSource(kafkaListingsConsumer).map(x => listingMapper(x))(rowListingType)
     val listingsTbl = tEnv.fromDataStream(listingsStream,
-      'earnest_payable_to,
+      'earnest_$_payable_to,
       'status_change_date,
       'inclusions,
       'county,
@@ -180,9 +145,12 @@ object TestJoiner {
       'colist_agent_id,
       'list_office_board_code,
       'list_207
-
     )
     tEnv.registerTable("listings_tbl", listingsTbl)
+
+    val resListings = tEnv.sqlQuery("SELECT * from listings_tbl")
+    val lRow: DataStream[Row] = tEnv.toAppendStream[Row](resListings)
+    lRow.print()
 
     val rowAgentType = new RowTypeInfo(
       Types.STRING, // trace_id
@@ -195,8 +163,8 @@ object TestJoiner {
     )
 
     def agentMapper(record: ObjectNode): Row = {
-      println("### agent record")
-      println(record)
+      //      println("### agent record")
+      //      println(record)
       val obj = record.get("value")
       return Row.of(
         obj.get("City").asText(),
@@ -210,7 +178,8 @@ object TestJoiner {
     }
 
     val agentsStream = env.addSource(kafkaAgentsConsumer).map(x => agentMapper(x))(rowAgentType)
-     val agentsTbl = tEnv.fromDataStream(agentsStream,
+
+    val agentsTbl = tEnv.fromDataStream(agentsStream,
       'city,
       'office_id,
       'email,
@@ -221,46 +190,30 @@ object TestJoiner {
     )
     tEnv.registerTable("agents_tbl", agentsTbl)
 
+    val resAgents = tEnv.sqlQuery("SELECT * from agents_tbl")
+    val aRow: DataStream[Row] = tEnv.toAppendStream[Row](resAgents)
+    aRow.print()
 
-//    val result = tEnv.sqlQuery(
-//      """SELECT l.earnest_payable_to,
-//         l.status_change_date,
-//         l.inclusions,
-//         l.county,
-//         l.agent_id,
-//         l.terms_offered,
-//         l.nbr_of_acres,
-//         l.colisting_member_url,
-//         l.colist_agent_id,
-//         l.list_office_board_code,
-//         l.list_207,
-//         a.city,
-//          a.office_id,
-//          a.email,
-//          a.re_negotiation_exp,
-//          a.nrdsid,
-//          a.mls_status,
-//          a.agent_id
-//FROM listings_tbl l
-//JOIN agents_tbl a ON l.agent_id = a.agent_id""".stripMargin)
-//
-//    tEnv.registerTable("JoinResult", result)
-//    tEnv.scan("JoinResult").toAppendStream[Row].print()
-
-//    val dsRow: DataStream[Row] = tEnv.toAppendStream[Row](result)
-//    dsRow.print()
+    val joinQuery =
+      """
+        | SELECT *
+        | FROM listings_tbl
+        | INNER JOIN agents_tbl
+        | ON listings_tbl.agent_id = agents_tbl.agent_id
+        | """.stripMargin
+    val result = tEnv.sqlQuery(joinQuery)
+    val row: DataStream[Row] = tEnv.toAppendStream[Row](result)
+    println("### result")
+    row.print()
 
     // Execute flow
     env.execute("Flink Joiner App")
 
 
-      }
-
-
-
+  }
+  
   private def getConsumerAndProducerProps(bootstrapServers: String): (Properties, Properties) = {
 
-    // Consumer properties: put together the broker list and a unique group id
     val consumerProps = new Properties
     consumerProps.setProperty("bootstrap.servers", bootstrapServers)
     consumerProps.setProperty("group.id", s"flink-kafka-test-${System.currentTimeMillis}")
