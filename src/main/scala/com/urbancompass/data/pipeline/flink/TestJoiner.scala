672ae1c6f14afa44d61fcc47e7bbaf38cb625789
reva-compass <rkandoji@compass.com>
2019-09-06 15:05:43 -0400

    nested left join
    - adding oh kafka source
    - using StreamExecutionEnvironment
    - performing nested join between listings, agents and oh

--- src/main/scala/com/urbancompass/data/pipeline/flink/TestJoiner.scala
+++ src/main/scala/com/urbancompass/data/pipeline/flink/TestJoiner.scala
@@ -1,38 +1,20 @@
 package com.urbancompass.data.pipeline.flink
 
-import java.util
 import java.util.Properties
-import java.sql.Timestamp
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.java.typeutils.RowTypeInfo
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala._
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
-import org.apache.flink.api.common.serialization.{AbstractDeserializationSchema, SimpleStringSchema}
-import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, FlinkKafkaProducer}
-import org.apache.flink.streaming.connectors.kafka
-import org.apache.flink.streaming.util.serialization.KeyedDeserializationSchema
-import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.api.scala._
-import org.apache.flink.table.api.scala._
-import org.apache.flink.table.descriptors.{Json, Kafka, Rowtime, Schema}
-import org.apache.flink.types.Row
-import org.apache.flink.table.api.{TableEnvironment, Types}
-import org.apache.flink.table.functions.TemporalTableFunction
 import org.apache.flink.runtime.state.filesystem.FsStateBackend
-import org.apache.flink.table.sources.tsextractors.ExistingField
-
-import scala.collection.mutable
-import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
-import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.api.java.typeutils.RowTypeInfo
-import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
-import org.apache.flink.formats.avro.AvroDeserializationSchema
-import org.apache.avro.generic.GenericRecord
-import org.apache.avro.specific.SpecificDatumReader
-import org.apache.avro.io.DecoderFactory
-import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
-import com.sksamuel.avro4s.{AvroInputStream, AvroName, AvroNamespace, AvroSchema}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
+import org.apache.flink.table.api.Types
+import org.apache.flink.table.api.scala._
+import org.apache.flink.types.Row
 
 object TestJoiner {
 
@@ -70,13 +52,15 @@ object TestJoiner {
     val bootstrapServers = params.getRequired("bootstrap-server")
     val kafkaListingsTopic = params.getRequired("listings-topic")
     val kafkaAgentsTopic = params.getRequired("agents-topic")
+    val kafkaOHTopic = params.getRequired("oh-topic")
     //val kafkaJoinedTopic = params.getRequired("sink-topic")
 
     /*
      Set up environment
      */
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val tEnv = TableEnvironment.getTableEnvironment(env)
+    val tEnv = StreamTableEnvironment.create(env)
+    //   val tEnv = TableEnvironment.getTableEnvironment(env)
     env.setStateBackend(new FsStateBackend("file:///Users/rkandoji/Documents/Software/flink-1.8.1/statebackend"))
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.setParallelism(1)
@@ -99,7 +83,11 @@ object TestJoiner {
     val kafkaAgentsConsumer = new FlinkKafkaConsumer[ObjectNode](kafkaAgentsTopic, jsonDeserdeSchema, consumerProps)
     kafkaAgentsConsumer.setStartFromEarliest()
 
+    val kafkaOHConsumer = new FlinkKafkaConsumer[ObjectNode](kafkaOHTopic, jsonDeserdeSchema, consumerProps)
+    kafkaOHConsumer.setStartFromEarliest()
+
     val rowListingType = new RowTypeInfo(
+      Types.STRING, // listing_id
       Types.STRING, // earnest_$_payable_to
       Types.STRING, // status_change_date
       Types.STRING, // inclusions
@@ -114,10 +102,9 @@ object TestJoiner {
     )
 
     def listingMapper(record: ObjectNode): Row = {
-      //      println("### listing object")
-      //      println(record)
       val obj = record.get("value")
       return Row.of(
+        if (obj.has("Listing ID")) obj.get("Listing ID").asText() else "",
         if (obj.has("Earnest $ Payable To")) obj.get("Earnest $ Payable To").asText() else "",
         if (obj.has("Status Change Date")) obj.get("Status Change Date").asText() else "",
         if (obj.has("Inclusions")) obj.get("Inclusions").asText() else "",
@@ -134,12 +121,14 @@ object TestJoiner {
 
 
     val listingsStream = env.addSource(kafkaListingsConsumer).map(x => listingMapper(x))(rowListingType)
-    val listingsTbl = tEnv.fromDataStream(listingsStream,
+    val keyedListingStream = listingsStream.keyBy(0)
+    val listingsTbl = tEnv.fromDataStream(keyedListingStream,
+      'listing_id,
       'earnest_$_payable_to,
       'status_change_date,
       'inclusions,
       'county,
-      'agent_id,
+      'l_agent_id,
       'terms_offered,
       'nbr_of_acres,
       'colisting_member_url,
@@ -150,8 +139,8 @@ object TestJoiner {
     tEnv.registerTable("listings_tbl", listingsTbl)
 
     //    val resListings = tEnv.sqlQuery("SELECT * from listings_tbl")
-    //    val lRow: DataStream[Row] = tEnv.toAppendStream[Row](resListings)
-    //    lRow.print()
+    ////    val lRow: DataStream[Row] = tEnv.toAppendStream[Row](resListings)
+    ////    lRow.print()
 
     val rowAgentType = new RowTypeInfo(
       Types.STRING, // trace_id
@@ -164,8 +153,6 @@ object TestJoiner {
     )
 
     def agentMapper(record: ObjectNode): Row = {
-      //      println("### agent record")
-      //      println(record)
       val obj = record.get("value")
       return Row.of(
         if (obj.has("City")) obj.get("City").asText() else "",
@@ -195,30 +182,91 @@ object TestJoiner {
     //    val aRow: DataStream[Row] = tEnv.toAppendStream[Row](resAgents)
     //    aRow.print()
 
+    val rowOHType = new RowTypeInfo(
+      Types.STRING, // trace_id
+      Types.STRING, // data_version
+      Types.STRING, // ts_created_at
+      Types.STRING, // __raw_ingest_start_time__
+      Types.STRING, // __uc_id_sha__
+      Types.STRING, // Agent ID
+      Types.STRING, // Agent First Name
+      Types.STRING // Agent First Name
+    )
+
+    def oHMapper(record: ObjectNode): Row = {
+      val obj = record.get("value")
+      return Row.of(
+        if (obj.has("City")) obj.get("City").asText() else "",
+        if (obj.has("Open House Comments")) obj.get("Open House Comments").asText() else "",
+        if (obj.has("Event End")) obj.get("Event End").asText() else "",
+        if (obj.has("Street Address")) obj.get("Street Address").asText() else "",
+        if (obj.has("Listing ID")) obj.get("Listing ID").asText() else "",
+        if (obj.has("Office Primary Phone")) obj.get("Office Primary Phone").asText() else "",
+        if (obj.has("Event Unique ID")) obj.get("Event Unique ID").asText() else "",
+        if (obj.has("Listing Agent Id")) obj.get("Listing Agent Id").asText() else ""
+      )
+    }
+
+    val ohStream = env.addSource(kafkaOHConsumer).map(x => oHMapper(x))(rowOHType)
+
+    val ohTbl = tEnv.fromDataStream(ohStream,
+      'oh_city,
+      'oh_comments,
+      'oh_event_end,
+      'oh_street_address,
+      'oh_listing_id,
+      'oh_office_primary_phone,
+      'oh_event_unique_id,
+      'oh_listing_agent_id
+    )
+    tEnv.registerTable("oh_tbl", ohTbl)
+
+    //    val resOH = tEnv.sqlQuery("SELECT * from oh_tbl")
+    //    val ohRow: DataStream[Row] = tEnv.toAppendStream[Row](resOH)
+    //    ohRow.print()
+
+
     //    val joinQuery =
     //      """
     //        | SELECT *
     //        | FROM listings_tbl
     //        | INNER JOIN agents_tbl
-    //        | ON listings_tbl.agent_id = agents_tbl.agent_idb
+    //        | ON listings_tbl.l_agent_id = agents_tbl.agent_id
     //        | """.stripMargin
     //    val result = tEnv.sqlQuery(joinQuery)
     //    val row: DataStream[Row] = tEnv.toAppendStream[Row](result)
     //    println("### INNER JOIN result")
     //    row.print()
 
+    //    val leftJoinQuery =
+    //      """
+    //        | SELECT *
+    //        | FROM listings_tbl l
+    //        | LEFT JOIN agents_tbl a
+    //        | ON l.agent_id = a.agent_id
+    //        | """.stripMargin
+    //    val leftResult = tEnv.sqlQuery(leftJoinQuery)
+    //    val leftJoinRow: DataStream[(Boolean, Row)] = tEnv.toRetractStream[Row](leftResult)
+    //    println("### LEFT JOIN result")
+    //    leftJoinRow.print()
 
-    val leftJoinQuery =
+    val nestedJoinQuery =
       """
+        | SELECT *
+        | FROM
+        | (
         | SELECT *
         | FROM listings_tbl l
         | LEFT JOIN agents_tbl a
-        | ON l.agent_id = a.agent_id
+        | ON l.l_agent_id = a.agent_id
+        | ) AS so
+        | LEFT JOIN oh_tbl AS oh
+        | ON so.listing_id = oh.oh_listing_id
         | """.stripMargin
-    val leftResult = tEnv.sqlQuery(leftJoinQuery)
-    val leftJoinRow: DataStream[(Boolean, Row)] = tEnv.toRetractStream[Row](leftResult)
-    println("### JOIN result")
-    leftJoinRow.print()
+    val nestedResult = tEnv.sqlQuery(nestedJoinQuery)
+    val nestedJoinRow: DataStream[(Boolean, Row)] = tEnv.toRetractStream[Row](nestedResult)
+    println("### NESTED JOIN result")
+    nestedJoinRow.print()
 
     // Execute flow
     env.execute("Flink Joiner App")
