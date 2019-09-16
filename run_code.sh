9726fbb57c8ebfcff5226e5aa37d91dc10955d58
reva-compass <rkandoji@compass.com>
2019-09-16 17:18:21 -0400

    duplicate issue workaround

--- run_code.sh
+++ run_code.sh
@@ -2,8 +2,8 @@
 
 /Users/rkandoji/Documents/Software/flink-1.8.1/bin/flink run target/scala-2.11/flink-joiner-assembly-0.1-SNAPSHOT.jar \
 --bootstrap-server  b-1.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-2.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-3.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-4.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-5.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-6.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092 \
---listings-topic poc_test_listing \
---agents-topic poc_test_agent \
+--listings-topic poc_test_listing_ts \
+--agents-topic poc_test_agent_ts \
 --oh-topic poc_test_oh \
 --sink-topic joined
 
