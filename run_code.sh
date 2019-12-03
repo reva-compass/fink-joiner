d463791e2cf1d82e533297a5eb510dd18ae71d5d
reva-compass <rkandoji@compass.com>
2019-12-03 13:58:38 -0500

    adding new dev cluster

--- run_code.sh
+++ run_code.sh
@@ -2,7 +2,7 @@
 
 /Users/rkandoji/Documents/Software/flink-1.8.1/bin/flink run target/scala-2.11/flink-joiner-assembly-0.1-SNAPSHOT.jar \
 --state-path "file:///Users/rkandoji/Documents/Software/flink-1.8.1/statebackend" \
---bootstrap-server  b-1.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-2.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-3.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-4.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-5.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092,b-6.listings-pipeline-beta.jlg1k0.c1.kafka.us-east-1.amazonaws.com:9092 \
+--bootstrap-server b-2.listings-infra-dev-191.lguuvv.c6.kafka.us-east-1.amazonaws.com:9092,b-1.listings-infra-dev-191.lguuvv.c6.kafka.us-east-1.amazonaws.com:9092,b-3.listings-infra-dev-191.lguuvv.c6.kafka.us-east-1.amazonaws.com:9092 \
 --listings-topic la_crmls_rets-listings-neo \
 --agents-topic la_crmls_rets-agents-neo \
 --oh-topic la_crmls_rets-openhouses-neo \
